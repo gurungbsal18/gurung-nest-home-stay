365de3582b47d59d31becbeb2be1f40f02972b80
gurungbsal18 <gurungbsal18@gmail.com>
2026-06-28 23:09:15 +0545

    color update of hero section

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -15,36 +15,39 @@ function HeroSection() {
     >
       {/* Black gradient overlay */}
       {/* <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/70" /> */}
-      <div className="absolute inset-0 bg-black/60" />
+      <div className="absolute inset-0 bg-black/65" />
 
       {/* Content */}
-      <div className="relative z-10 container mx-auto flex h-full w-full lg:w-7xl flex-col items-center justify-center gap-8 text-white">
+      <div className="relative z-10 container mx-auto flex h-full w-full flex-col items-center justify-center gap-8 text-white lg:w-7xl">
         <div className="flex flex-col items-center gap-2 text-center">
-          <p className="rounded-lg bg-yellow-500/60 px-4 py-2 text-xs font-semibold flex items-center gap-1">
-           <GrLocation/> RAMKOT, KATHMANDU
+          <p className="flex items-center gap-1 rounded-lg bg-yellow-500/60 px-4 py-2 text-xs font-semibold">
+            <GrLocation /> RAMKOT, KATHMANDU
           </p>
-          <h2 className="text-3xl md:text-4xl lg:text-6xl font-bold">Escape the City</h2>
-          <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
-            Experience Kathmandu from Above
+          <h2 className="text-3xl font-bold md:text-4xl lg:text-6xl">
+            Escape the City
           </h2>
-          <p className="mt-4 w-full lg:w-5xl text-base md:text-xl">
+          <h2 className="text-2xl font-bold md:text-4xl lg:text-6xl">
+            <span className="text-primary">Experience Kathmandu</span> from
+            Above
+          </h2>
+          <p className="mt-4 w-full text-base md:text-xl lg:w-5xl">
             A peaceful hilltop homestay in Ramkot featuring breathtaking
             panoramic views of Kathmandu Valley, modern comfort, and
             unforgettable sunsets.
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-2 md:gap-4">
+        <div className="flex flex-col gap-2 md:flex-row md:gap-4">
           <Link
             href=""
-            className="flex gap-2 h-8 md:h-16 items-center justify-center rounded-xl bg-primary px-8 py-6 duration-500 ease-in-out hover:bg-primary/80"
+            className="flex h-8 items-center justify-center gap-2 rounded-xl bg-primary px-8 py-6 duration-500 ease-in-out hover:bg-primary/80 md:h-16"
           >
-<LuCalendarDays size={"24"}/>
+            <LuCalendarDays size={"24"} />
             Book Your Stay
           </Link>
           <Link
             href=""
-            className="flex h-8 md:h-16 items-center justify-center rounded-xl border border-white px-8 py-6 duration-500 ease-in-out hover:border-primary hover:bg-primary"
+            className="flex h-8 items-center justify-center rounded-xl border border-white px-8 py-6 duration-500 ease-in-out hover:border-primary hover:bg-primary md:h-16"
           >
             Explore Rooms
           </Link>
