4e7bf32d43ed4b77baf73d083ac468ac1151abe8
Bishal Gurung <gurungbsal18@gmail.com>
2026-06-28 09:48:12 +0545

    responsive update

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { LuCalendarDays } from "react-icons/lu";
+import { GrLocation } from "react-icons/gr";
 
 function HeroSection() {
   return (
@@ -17,23 +18,23 @@ function HeroSection() {
       <div className="absolute inset-0 bg-black/60" />
 
       {/* Content */}
-      <div className="relative z-10 container mx-auto flex h-full w-7xl flex-col items-center justify-center gap-8 text-white">
+      <div className="relative z-10 container mx-auto flex h-full w-full lg:w-7xl flex-col items-center justify-center gap-8 text-white">
         <div className="flex flex-col items-center gap-2 text-center">
-          <p className="rounded-lg bg-yellow-500/60 px-4 py-2 text-xs font-semibold">
-            RAMKOT, KATHMANDU
+          <p className="rounded-lg bg-yellow-500/60 px-4 py-2 text-xs font-semibold flex items-center gap-1">
+           <GrLocation/> RAMKOT, KATHMANDU
           </p>
-          <h2 className="text-6xl font-bold">Escape the City</h2>
-          <h2 className="text-6xl font-bold">
+          <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">Escape the City</h2>
+          <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
             Experience Kathmandu from Above
           </h2>
-          <p className="mt-4 w-5xl text-xl">
+          <p className="mt-4 w-full lg:w-5xl text-base md:text-xl">
             A peaceful hilltop homestay in Ramkot featuring breathtaking
             panoramic views of Kathmandu Valley, modern comfort, and
             unforgettable sunsets.
           </p>
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex flex-col md:flex-row gap-2 md:gap-4">
           <Link
             href=""
             className="flex gap-2 h-16 items-center justify-center rounded-xl bg-primary px-8 py-6 duration-500 ease-in-out hover:bg-primary/80"
