d4998b53a8b0ef55cc7ab260e324535d00538f07
Bishal Gurung <gurungbsal18@gmail.com>
2026-07-02 08:52:53 +0545

    form added

--- frontend/components/BookingForm.tsx
+++ frontend/components/BookingForm.tsx
@@ -10,6 +10,9 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { Textarea } from "./ui/textarea"
 
 export function BookingForm() {
   const [open, setOpen] = React.useState(false)
@@ -17,7 +20,7 @@ export function BookingForm() {
 
   return (
     <div className="flex w-full justify-center">
-      <form className="w-2xl rounded-md border border-gray-200 p-8 shadow-xl">
+      <form className="flex w-2xl flex-col gap-4 rounded-md border border-gray-200 p-8 shadow-xl">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
           <Field className="mx-auto w-full">
             <FieldLabel htmlFor="date">Check-in</FieldLabel>
@@ -77,6 +80,35 @@ export function BookingForm() {
               </PopoverContent>
             </Popover>
           </Field>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Label>Guests</Label>
+          <Input type="number" />
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex flex-col gap-2">
+            <Label>First Name</Label>
+            <Input type="text" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Last Name</Label>
+            <Input type="text" />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Label>Phone Number</Label>
+          <Input type="number" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <Label>Special Requests</Label>
+          <Textarea />
+        </div>
+        <div className="w-full">
+          <Button className="w-full">Submit</Button>
         </div>
       </form>
     </div>
--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -4,12 +4,12 @@ function Footer() {
   return (
     <div className="border border-t-gray-200">
       <div className="container mx-auto py-16">
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-16 lg:gap-24">
-          <div className="col-span-1 lg:col-span-2 flex flex-col gap-2">
+        <div className="grid grid-cols-1 gap-16 lg:grid-cols-4 lg:gap-24">
+          <div className="col-span-1 flex flex-col gap-2 lg:col-span-2">
             <p className="font-heading text-2xl font-bold text-primary">
               Gurung Nest Homestay
             </p>
-            <p className="text-gray-500">
+            <p className="text-sm text-gray-500">
               A peaceful hilltop homestay in Ramkot, Kathmandu offering
               breathtaking panoramic views of Kathmandu Valley, modern comfort,
               and unforgettable sunsets.
@@ -33,7 +33,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between border-t border-gray-200 py-4 text-sm text-gray-500">
+      <div className="container mx-auto flex flex-col items-center justify-between border-t border-gray-200 py-4 text-sm text-gray-500 lg:flex-row">
         <p>&copy; Gurung Nest Homestay. All rights reserved.</p>
         <p>Crafted with care above Kathmandu Valley</p>
       </div>
